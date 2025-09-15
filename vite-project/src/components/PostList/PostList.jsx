48d9cc62f732b2b025578f9fff23cfbce44f00bf
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-09-15 19:11:41 -0300

    feat: добавила логику вывода уникального номера для каждого поста. index + 1 чтобы не начинался с нуля

--- vite-project/src/components/PostList/PostList.jsx
+++ vite-project/src/components/PostList/PostList.jsx
@@ -3,8 +3,8 @@ export default function PostList({posts, title}) {
     return (
         <>
          <h1 style={{color: 'blue', textAlign: 'center'}}>{title}</h1>
-        {posts.map(item => (
-          <PostItem post={item} key={item.id}/>
+        {posts.map((item, index) => (
+          <PostItem number={index + 1} post={item} key={item.id}/>
         ))}
         </>
     )
