7d16cb8627ebf01462a0f499d7d948126a011d7e
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-09-15 16:51:51 -0300

    feat: вынесла массив элеменов обратно в App и сделала возможность динамического отображение заголовка, теперь можно составлять несколько независимых блоков постов

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import PostList from './components/PostList/PostList'
-
+import { useState } from 'react'
 function App() {
-  
+     const [posts, setPosts] = useState([
+    {id: 1, title: " JavaScript", body: "description"},
+    {id: 2, title: " TypeScript", body: "description"},
+    {id: 3, title: " Redux", body: "description"},
+    {id: 4, title: " React", body: "description"}
+  ])
+
   return (
     <div className="App">
-     <PostList />
+     <PostList posts={posts} title={"Список постов 1"}/>
      </div>
   )
 }
--- vite-project/src/components/PostList/PostList.jsx
+++ vite-project/src/components/PostList/PostList.jsx
@@ -1,17 +1,8 @@
-import {useState} from 'react'
 import PostItem from '../PostItem/PostItem'
-export default function PostList() {
-
-    const [posts, setPosts] = useState([
-    {id: 1, title: " JavaScript", body: "description"},
-    {id: 2, title: " TypeScript", body: "description"},
-    {id: 3, title: " Redux", body: "description"},
-    {id: 4, title: " React", body: "description"}
-  ])
-
+export default function PostList({posts, title}) {
     return (
         <>
-         <h1 style={{color: 'blue', textAlign: 'center'}}>Список постов</h1>
+         <h1 style={{color: 'blue', textAlign: 'center'}}>{title}</h1>
         {posts.map(item => (
           <PostItem post={item} key={item.id}/>
         ))}
