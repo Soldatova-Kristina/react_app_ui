381cec5d8adb51f14244bf8108adfa3de84eaaf8
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-09-15 16:54:59 -0300

    feat: реализация наглядного отображения что содержимое PostList меняется динамически

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import PostList from './components/PostList/PostList'
 import { useState } from 'react'
+
 function App() {
      const [posts, setPosts] = useState([
     {id: 1, title: " JavaScript", body: "description"},
@@ -9,9 +10,16 @@ function App() {
     {id: 4, title: " React", body: "description"}
   ])
 
+     const [posts2, setPosts2] = useState([
+    {id: 1, title: " Git", body: "description"},
+    {id: 2, title: " CI/CD", body: "description"},
+    {id: 3, title: " UI Kit", body: "description"},
+  ])
+
   return (
     <div className="App">
      <PostList posts={posts} title={"Список постов 1"}/>
+      <PostList posts={posts2} title={"Список постов 2"}/>
      </div>
   )
 }
