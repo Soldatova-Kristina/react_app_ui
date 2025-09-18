c04153ac8167df48df02a7e4e4688484f2607406
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-09-18 17:18:20 -0300

    feat: реализовала функциональность отображения определенного кличества постов на странице

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -14,11 +14,15 @@ function App() {
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false);
+  const [totalCount, setTotalCount]= useState(0);
+  const [limit, setLimit]= useState(10)
+  const [page, setPage]= useState(1)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
  
   const [getPosts, isPostLoading, postError] = useFetching( async ()=> {
-      const posts = await PostService.getAll()
-      setPosts(posts)
+      const response = await PostService.getAll(limit, page)
+      setPosts(response.data)
+      setTotalCount(response.headers['x-total-count'])
   })
 
   useEffect (() => {
