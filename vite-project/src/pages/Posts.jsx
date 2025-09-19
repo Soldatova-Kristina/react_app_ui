9963e7a679dd755e307fc5c7400003406bce2186
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-09-19 18:24:59 -0300

    feat: реализовала бесконечную ленту

--- vite-project/src/pages/Posts.jsx
+++ vite-project/src/pages/Posts.jsx
@@ -1,5 +1,5 @@
 import PostList from '../components/PostList/PostList'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import PostForm from '../components/PostForm/PostForm'
 import PostFilter from '../components/PostFilter/PostFilter'
 import MyModal from '../components/MyModal/MyModal'
@@ -11,6 +11,7 @@ import useFetching from '../hooks/useFetching'
 import { getPageCount } from '../utils/pages'
 import usePagination from '../hooks/usePagination'
 import Pagination from '../components/Pagination/Pagination'
+import { useObserver } from '../hooks/useObserver'
 
 function Posts() {
   const [posts, setPosts] = useState([])
@@ -21,13 +22,23 @@ function Posts() {
   const [page, setPage]= useState(1)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
   const { pages } = usePagination(totalPages)
-    
+  const lastElementRef = useRef()
+
   const [getPosts, isPostLoading, postError] = useFetching( async ()=> {
       const response = await PostService.getAll(limit, page)
-      setPosts(response.data)
+      setPosts([...posts, ...response.data])
       const totalCount = response.headers['x-total-count'];
       setTotalPages(getPageCount(totalCount, limit))
   })
+
+  useObserver(lastElementRef, page < totalPages, isPostLoading, () => {
+    setPage(page + 1)
+  })
+
+   useEffect(() => {
+    getPosts(limit, page)
+  }, [page]) 
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -42,9 +53,7 @@ function Posts() {
     setPage(newPage)
   }
 
-  useEffect(() => {
-    getPosts()
-  }, [page]) 
+ 
  
   return (
     <div className="App">
@@ -62,18 +71,20 @@ function Posts() {
      filter={filter}
      setFilter={setFilter}
      />
-     {postError && <h1>Произошла ошибка ${postError}</h1> }
-     {isPostLoading 
-     ? <div style={{display: "flex", justifyContent: "center", marginTop: 50}}><Loader /></div>
-     : <>
-         <PostList deletePost={deletePost} posts={sortedAndSearchedPosts} title={"Список постов"} />
+     {postError && 
+     <h1>Произошла ошибка ${postError}</h1>
+      }
+     <PostList deletePost={deletePost} posts={sortedAndSearchedPosts} title={"Список постов"} />
+     <div ref={lastElementRef}></div>
+     {isPostLoading && 
+     <div style={{display: "flex", justifyContent: "center", marginTop: 50}}><Loader /></div>
+     }
+         
          <Pagination 
            pages={pages}
            currentPage={page}
            onPageChange={changePage}
          />
-       </>
-     }
     </div>
   )
 }
