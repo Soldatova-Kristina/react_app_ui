79f57cfb25c5514696348de3e81304bf00bf81af
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-09-19 15:23:12 -0300

    feat: заменён устаревший useHistory на useNavigate для маршрутизации постов
    Причина:
    - В проекте использовался хук useHistory из react-router-dom, который был удалён начиная с версии 6.
    - При попытке использовать useHistory возникала ошибка: 'does not provide an export named useHistory'.
    - В React Router v6 вместо useHistory нужно использовать useNavigate.
    
    Что сделано:
    1. В компоненте PostItem заменён импорт:
       - было: import { useHistory } from 'react-router-dom'
       - стало: import { useNavigate } from 'react-router-dom'
    
    2. Логика навигации переписана:
       - было: router.push()
       - стало: navigate()
    
    3. Теперь при клике на кнопку 'Открыть пост':
       - useNavigate возвращает функцию navigate;
       - вызов navigate('/posts/:id') перенаправляет пользователя на нужный маршрут.
    
    Итог:
    - Ошибка с отсутствием useHistory устранена.
    - Код соответствует современному API react-router-dom v6.
    - Поведение осталось тем же: кнопка 'Открыть пост' ведёт на страницу выбранного поста.

--- vite-project/src/components/PostItem/PostItem.jsx
+++ vite-project/src/components/PostItem/PostItem.jsx
@@ -1,6 +1,10 @@
 import MyButton from "../../UI/button/MyButton";
+import { useNavigate} from 'react-router-dom'
 
 const PostItem =  (props) => {
+
+  const navigate = useNavigate()
+
     return (
         <div className="post">
         <div className="post__content">
@@ -10,6 +14,7 @@ const PostItem =  (props) => {
           </div>
         </div>
         <div className="post__btns">
+          <MyButton onClick={() => navigate(`/posts/${props.post.id}`)}>Открыть</MyButton>
           <MyButton onClick={() => props.deletePost(props.post)}>Удалить</MyButton>
         </div>
       </div>
