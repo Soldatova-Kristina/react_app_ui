0db60a817ed74a88fa269d11c9d7ec5d7bcd3c43
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-09-19 14:47:37 -0300

    feat: настроила маршрут по умолчанию

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,18 @@
 import './App.css' 
 import Posts from './pages/Posts'
 import About from './pages/About'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import NavBar from './UI/navbar/NavBar';
 
 
 function App() {
   return (
     <Router>
-      <div className="navbar">
-        <div className='navbar__links'>
-          <Link to="/about">About</Link>
-          <Link to="/posts">Posts</Link>
-        </div>
-      </div>
+      <NavBar />
       <Routes>
         <Route path="/posts" element={<Posts />} />
         <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Navigate to="/posts" replace />} />
       </Routes>
     </Router>
   );
