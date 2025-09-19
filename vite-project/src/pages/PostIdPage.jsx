bc234bd4750124d59279b7a2e05a2b406ca1d7f7
Soldatova-Kristina <soldatovadev98@icloud.com>
2025-09-19 16:31:23 -0300

    feat: добавила загрузку комментариев к посту

--- vite-project/src/pages/PostIdPage.jsx
+++ vite-project/src/pages/PostIdPage.jsx
@@ -7,14 +7,21 @@ import Loader from '../UI/Loader/Loader'
 const PostIdPage = () => {
   const params = useParams()
   const [post, setPost] = useState({})
-  
+  const [comm, setComm] = useState([])
+
   const [fetchPostbyId, isPostLoading, postError] = useFetching(async () => {
     const response = await PostService.getById(params.id);
     setPost(response.data)
   })
 
+  const [fetchCommbyId, isCommLoading, comError] = useFetching(async () => {
+    const response = await PostService.getComId(params.id);
+    setComm(response.data)
+  })
+
   useEffect(() => {
     fetchPostbyId()
+    fetchCommbyId()
   }, [params.id])
 
   return (
@@ -23,9 +30,17 @@ const PostIdPage = () => {
        {postError && <h1>Произошла ошибка: {postError}</h1>}
        {isPostLoading
        ? <Loader />
-       : <div>{post.id && <><h3>{post.title}</h3><p>{post.body}</p></>}</div> }
-    </div>
+       : <div>{post.id && <><p>Описание поста: {post.title}</p></>}</div> }
+       {isCommLoading
+       ? <Loader />
+       : <div> {comm.map(com => (
+          <div key={com.id}>Комментарии к посту:
+           <p>Email комментатора: {com.email}</p>
+            <p>Комментарий: {com.body}</p>
+          </div>))}
+           </div>
+}   
+</div>
   )
 }
-
-export default PostIdPage
+export default PostIdPage;
